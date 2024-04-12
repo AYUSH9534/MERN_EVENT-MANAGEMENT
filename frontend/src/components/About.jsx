aab0747cfc2df7b47f82b331f9958cc2d4ee0f20
Ayush Arun <ayusharun4@gmail.com>
2024-04-12 18:21:14 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events Details:-</h4>
+      <h2>ABOUT</h2>
+      <p>
+      At <b>RajSphere</b>,we believe every event should be a mosaic of happy moments, each one tailored to bring smiles and joy to all who attend. Nestled in the heart of the city, our venue offers a warm, inviting atmosphere perfect for any occasion—be it a lively birthday celebration, a jubilant anniversary, or a vibrant corporate event. Our dedicated team of event planners is committed to understanding your vision and transforming it into a reality filled with laughter, cheer, and unforgettable experiences.
+      Let us help you celebrate life’s milestones and create joyous memories that will last a lifetime. Reach out today to start planning with <b>RajSphere</b>, where every gathering is a celebration of happiness. We’re excited to work with you, bringing creativity, passion, and excellence to your special occasion, making sure every moment is not just planned but sincerely cherished.
+      </p>
+    </section>
+  );
+};
+
+export default About;
