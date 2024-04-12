c9f3808ab7b753c5d1d31d2d9aabe129b46019b4
Ayush Arun <ayusharun4@gmail.com>
2024-04-12 18:39:02 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,13 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req,res) =>{
+  res.json({
+    success: true,
+    message: "Welcome to this site",
+  });
+});
+
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
